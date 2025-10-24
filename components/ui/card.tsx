42a9c30279d133784366b14867fd3c524ad3df86
d-liya <dilum14@gmail.com>
2025-10-24 19:32:35 -0230

    updated styles

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,7 +7,7 @@ const Card = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("border bg-background text-card-foreground", className)}
+    className={cn("border bg-card text-card-foreground", className)}
     {...props}
   />
 ));
