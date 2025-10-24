25d3d33684501ff6236745a75ee192227c6799d3
d-liya <dilum14@gmail.com>
2025-10-24 19:30:18 -0230

    styles improved

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -44,7 +44,11 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <p ref={ref} className={cn("text-sm text-[#606060]", className)} {...props} />
+  <p
+    ref={ref}
+    className={cn("text-sm text-card-foreground", className)}
+    {...props}
+  />
 ));
 CardDescription.displayName = "CardDescription";
 
@@ -63,7 +67,7 @@ const CardFooter = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "flex items-center p-6 border-t text-xs text-[#606060]",
+      "flex items-center p-6 border-t text-xs text-card-foreground",
       className
     )}
     {...props}
